d5dee2e995605e2fd6343a240b0f19f8365cff03
George Popovic <georgesmpopovic@gmail.com>
2023-05-09 13:28:40 -0400

    add open AI

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -7,13 +7,56 @@ use BotMan\BotMan\BotMan;
 use BotMan\BotMan\BotManFactory;
 use BotMan\BotMan\Drivers\DriverManager;
 
+use Orhanerday\OpenAi\OpenAi;
+
 class BotController extends Controller
 {
     //
 
 
-    public function index(){
+    public function chat($question){
+
+        $open_ai_key = getenv('OPENAI_API_KEY');
+        $open_ai = new OpenAi($open_ai_key);
+
+        $complete = $open_ai->chat([
+            'model' => 'gpt-3.5-turbo',
+            'messages' => [
+                [
+                    "role" => "system",
+                    "content" => "You are a helpful assistant."
+                ],
+                [
+                    "role" => "user",
+                    "content" => "Who won the world series in 2020?"
+                ],
+                [
+                    "role" => "assistant",
+                    "content" => "The Los Angeles Dodgers won the World Series in 2020."
+                ],
+                [
+                    "role" => "user",
+                    "content" => "Where was it played?"
+                ],
+            ],
+            'temperature' => 1.0,
+            'max_tokens' => 4000,
+            'frequency_penalty' => 0,
+            'presence_penalty' => 0,
+         ]);
+
+         // decode response
+        $d = json_decode($complete);
+
+        // Get Content
         
+        error_log($complete);
+        //return $d->choices[0]->message->content;
+
+    }
+
+    public function index(){
+
         $config = [
             // Your driver-specific configuration
             // "telegram" => [
@@ -29,7 +72,8 @@ class BotController extends Controller
         
         // Give the bot something to listen for.
         $botman->hears("{question}", function (BotMan $bot, $question) {
-            $bot->reply($question);
+            $content = $this->chat($question);
+            $bot->reply($content);
         });
         
         // Start listening
