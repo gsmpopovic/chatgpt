0e9675947218b8d1414512adaa65034312880ab2
George Popovic <georgesmpopovic@gmail.com>
2023-05-09 19:01:48 -0400

    remove iframe endpoint

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,8 @@
 <script>
 var botmanWidget = {
-    frameEndpoint: {{$route}}
+    // frameEndpoint: "localhost:8000/chat",
+    introMessage: "Hiya! I'm powered by ChatGPT. Whatcha got for me?",
+    title: "Customer Support"
 };
 </script>
 <script src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/widget.js'></script>
