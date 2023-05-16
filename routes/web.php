9a917108a713874141142c03ba3a6d65d39dd57a
George Popovic <georgesmpopovic@gmail.com>
2023-05-16 14:23:02 -0400

    install jetstream with vuejs/inertia + fix web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 use App\Http\Controllers\BotController;
 
 /*
@@ -13,6 +15,25 @@ use App\Http\Controllers\BotController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+});
 
 Route::get('/', function () {
 
@@ -28,3 +49,4 @@ Route::get('/chat', function () {
 })->name('chat');
 
 Route::post('/botman', [BotController::class, 'index']);
+
