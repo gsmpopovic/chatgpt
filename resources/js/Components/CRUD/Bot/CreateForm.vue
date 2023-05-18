d35b39a87b4849a4cb934caf2d846b6f9d64387e
George Popovic <georgesmpopovic@gmail.com>
2023-05-18 13:22:55 -0400

    form values in obj and reformatted

--- resources/js/Components/CRUD/Bot/CreateForm.vue
+++ resources/js/Components/CRUD/Bot/CreateForm.vue
@@ -5,15 +5,63 @@ import AuthenticationCardLogo from '@/Components/AuthenticationCardLogo.vue';
 import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
+import InputElement from '@/Components/InputElement.vue';
 
 defineProps({
     status: String,
 });
 
-const form = useForm({
+const formValues = {
     title: '',
-});
+    chat_server:'/botman',
+    iframe_endpoint:'/botman/chat',
+    time_format:'',
+    datetime_format:'',
+    title:'',
+    intro_message:'',
+    display_message_time:'',
+    placeholder_text:'',
+    main_color:'',
+    bubble_avatar_url:'',
+    desktop_height:'',
+    desktop_width:'',
+    mobile_height:'',
+    mobile_width:'',
+    video_height:'',
+    about_link:'',
+};
+
+const normalizeInputName = function(string){
+    
+    let name = string.split('_').join(' ');
+    
+    return name; 
+
+}
+
+const setupFormElements = function(obj){
+
+    let elems = [];
+
+    for (const [key, value] of Object.entries(obj)) {
+        
+        let elem = {};
+        elem.label = normalizeInputName(key); 
+
+        elem.name = key; 
+        elem.value = value;
+        elem.type = 'text';
+        elems.push(elem);
+    }
+
+    return elems; 
+
+};
+
+const formElements = setupFormElements(formValues);
+
+
+const form = useForm(formValues);
 
 const submit = () => {
     form.post(route('bot.store'));
@@ -36,18 +84,18 @@ const submit = () => {
         </div>
 
         <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="title" value="title" />
-                <TextInput
-                    id="title"
-                    v-model="form.title"
+            <div v-for="elem in formElements">
+                <InputLabel :for="elem.name" :value="elem.label" />
+                <InputElement
+                    :id="elem.name"
+                    v-model="form[elem.name]"
                     type="text"
                     class="mt-1 block w-full"
                     required
                     autofocus
-                    autocomplete="username"
                 />
-                <InputError class="mt-2" :message="form.errors.title" />
+                <InputError class="mt-2" :message="form.errors[elem.name]" />
+                <br>
             </div>
 
             <div class="flex items-center justify-end mt-4">
